b89a9d1a8f3f5e736f612bb37e6fab8c97b8c793
neglux <akcaaytuna@gmail.com>
2022-10-13 14:21:23 +0300

    adding custom scrollbar

--- src/components/styles/Global.js
+++ src/components/styles/Global.js
@@ -13,4 +13,20 @@ export const GlobalStyles = createGlobalStyle`
         color: ${({ theme }) => theme.colors.text};
         background-color: ${({ theme }) => theme.colors.body};
     }
+
+    ::-webkit-scrollbar {
+    width: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    }
+
+    ::-webkit-scrollbar-thumb {
+    background: #888;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+    }
 `;
