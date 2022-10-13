6e3003dbd23b13f07520db9dccd34bb20744afe8
neglux <akcaaytuna@gmail.com>
2022-10-13 14:19:39 +0300

    removing border

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -5,7 +5,6 @@ export const StyledHeader = styled.header`
   justify-content: center;
   padding: 1rem 0;
 
-  border-bottom: 2px solid ${({ theme }) => theme.colors.headerBorder};
   h1 {
     font-size: 1.4rem;
     font-weight: 500;
