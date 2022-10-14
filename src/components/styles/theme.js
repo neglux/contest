c83860dc2cf9afb054d3a5eaa8aab7510efabad2
neglux <akcaaytuna@gmail.com>
2022-10-14 17:37:23 +0300

    adding theme

---
+++ src/components/styles/theme.js
@@ -0,0 +1,13 @@
+import { palettes } from "./palettes";
+
+export const theme = {
+  colors: {
+    text: palettes.palette1.colorTextLight,
+    linkText: palettes.palette1.colorDarkenTextLight,
+    linkHover: palettes.palette1.colorTextLight,
+    body: palettes.palette1.colorSecondary,
+    contestBg: palettes.palette1.colorTextDark,
+    logoBg: palettes.palette1.white,
+    labelBg: palettes.palette1.colorSecondary,
+  },
+};
