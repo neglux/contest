7bc5d4609985de08a0f219d91fcc03740e4d3f00
neglux <akcaaytuna@gmail.com>
2022-10-15 09:28:26 +0300

    adding active tab style

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyledTabs } from "./styles/Tabs.styled";
 
 const Tabs = ({ data, setFilter }) => {
+  const [active, setActive] = useState(false);
+
+  const activeStyle = {
+    color: "#EEEEEE",
+  };
+
   return (
     <StyledTabs className="tabs__list">
       {data.map((item, index) => (
         <li
           key={index}
           className="tabs__item"
+          style={active === index ? activeStyle : {}}
           onClick={() => {
+            setActive(index);
             setFilter(item.toLowerCase());
           }}
         >
