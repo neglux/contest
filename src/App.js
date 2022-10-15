d428366f09ab01b0341ae598689ca19ebf3b5bfd
neglux <akcaaytuna@gmail.com>
2022-10-15 06:40:02 +0300

    adding data filtering

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { ThemeProvider } from "styled-components";
 import { theme } from "./components/styles/theme";
 
@@ -7,17 +9,19 @@ import { Container } from "./components/styles/Container.styled";
 import { Grid } from "./components/styles/Grid.styled";
 
 import Header from "./components/Header";
+import Tabs from "./components/Tabs";
 import Contest from "./components/Contest";
+import Loading from "./components/Loading";
 import Footer from "./components/Footer";
 
 import useFetch from "./hooks/useFetch";
 
 import { dataHandler } from "./handlers/dataHandler";
-import Loading from "./components/Loading";
+import { filterHandler } from "./handlers/filterHandler";
 
 function App() {
   const { isLoading, data } = useFetch("https://kontests.net/api/v1/all");
-
+  const [filter, setFilter] = useState("all");
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
@@ -27,12 +31,15 @@ function App() {
           {isLoading ? (
             <Loading />
           ) : (
-            <Grid>
-              {dataHandler(data).map(
-                (contest, index) =>
-                  contest && <Contest key={index} {...contest} />
-              )}
-            </Grid>
+            <>
+              <Tabs data={["Active", "Upcoming"]} setFilter={setFilter} />
+              <Grid>
+                {filterHandler(dataHandler(data), filter).map(
+                  (contest, index) =>
+                    contest && <Contest key={index} {...contest} />
+                )}
+              </Grid>
+            </>
           )}
         </Container>
         <Footer />
