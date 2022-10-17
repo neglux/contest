219c0f2463535f623444616cc43d821a27605b66
neglux <akcaaytuna@gmail.com>
2022-10-17 13:39:17 +0300

    removing error img

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,12 +1,10 @@
 import React from "react";
-import errorImg from "../img/error.svg";
 import { StyledError } from "./styles/Error.styled";
 
 const Error = ({ msg }) => {
   return (
     <StyledError>
       <h3 className="error__msg">{msg}</h3>
-      <img className="error__img" src={errorImg} alt="error img" />
     </StyledError>
   );
 };
