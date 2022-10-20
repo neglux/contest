625c2ca07946c62a2fdb9d87e6880d9e95aeefcc
neglux <akcaaytuna@gmail.com>
2022-10-20 14:10:10 +0300

    style fixes

--- src/components/Modal.styled.js
+++ src/components/Modal.styled.js
@@ -2,12 +2,14 @@ import styled from "styled-components";
 
 export const StyledModal = styled.div`
   position: absolute;
-  top: -250%;
-  left: -10%;
-
+  bottom: 0;
+  left: 0;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
   .modal {
     &__list {
       list-style: none;
+      text-transform: lowercase;
 
       padding: 0.5rem;
       font-size: 0.9rem;
